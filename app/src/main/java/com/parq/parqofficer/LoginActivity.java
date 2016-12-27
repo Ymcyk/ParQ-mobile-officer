667dc41285775d7e02df61d33f6e5330a53125e1
Piotr Zieliński <thepateto@gmail.com>
2016-12-27 02:42:17 +0100

    Disabling and enabling button depends on labels

--- app/src/main/java/com/parq/parqofficer/LoginActivity.java
+++ app/src/main/java/com/parq/parqofficer/LoginActivity.java
@@ -2,12 +2,54 @@ package com.parq.parqofficer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText usernameLabel;
+    private EditText passwordLabel;
+    private Button loginButton;
+
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            checkLabelsForEmptyValues();
+        }
+    };
+
+    private void checkLabelsForEmptyValues() {
+        String username = usernameLabel.getText().toString();
+        String password = passwordLabel.getText().toString();
+
+        if(username.isEmpty() || password.isEmpty())
+            loginButton.setEnabled(false);
+        else
+            loginButton.setEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameLabel = (EditText) findViewById(R.id.username_label);
+        passwordLabel = (EditText) findViewById(R.id.password_label);
+        loginButton = (Button) findViewById(R.id.login_button);
+
+        usernameLabel.addTextChangedListener(loginTextWatcher);
+        passwordLabel.addTextChangedListener(loginTextWatcher);
+    }
+
+    public void loginOnClick(View view) {
+
     }
 }
