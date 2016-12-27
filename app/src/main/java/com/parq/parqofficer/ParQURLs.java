7488191fbe52209093d239e31ae6f8179f7b204e
Piotr Zieliński <thepateto@gmail.com>
2016-12-27 22:30:28 +0100

    New classes for handling urls and token authorization

---
+++ app/src/main/java/com/parq/parqofficer/ParQURLs.java
@@ -0,0 +1,31 @@
+package com.parq.parqofficer;
+
+import android.net.Uri.Builder;
+
+/**
+ * Created by piotr on 27.12.16.
+ */
+
+public class ParQURLs {
+    private static String authority;
+
+    public static String getAuthority() {
+        return authority;
+    }
+
+    public static void setAuthority(String authority) {
+        ParQURLs.authority = authority;
+    }
+
+    private static Builder getBase() {
+        return new Builder()
+                .scheme("http")
+                .encodedAuthority(ParQURLs.authority);
+    }
+
+    public static String getLoginURL() {
+        Builder builder = ParQURLs.getBase()
+                .appendEncodedPath("api-token-auth/");
+        return builder.build().toString();
+    }
+}
---
+++ app/src/main/java/com/parq/parqofficer/TokenAuth.java
@@ -0,0 +1,13 @@
+package com.parq.parqofficer;
+
+/**
+ * Created by piotr on 27.12.16.
+ */
+
+public class TokenAuth {
+    private static String Token;
+
+    private void requestToken() {
+
+    }
+}
