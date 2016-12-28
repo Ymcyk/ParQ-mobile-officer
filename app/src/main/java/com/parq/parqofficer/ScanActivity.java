36ecca85b893eb7597c7dfe03c0241ac60a3986d
Piotr Zieliński <thepateto@gmail.com>
2016-12-28 13:54:31 +0100

    Scanning code

--- app/src/main/java/com/parq/parqofficer/ScanActivity.java
+++ app/src/main/java/com/parq/parqofficer/ScanActivity.java
@@ -1,8 +1,13 @@
 package com.parq.parqofficer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class ScanActivity extends AppCompatActivity {
 
@@ -13,6 +18,19 @@ public class ScanActivity extends AppCompatActivity {
     }
 
     public void startScanOnClick(View view) {
+        IntentIntegrator integrator = new IntentIntegrator(this)
+                .setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES)
+                .setPrompt("Scan");
+        integrator.initiateScan();
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null && result.getContents() != null){
+            Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+        }
     }
 }
